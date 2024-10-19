b6f0121cf473f51b1872e94cad0c398ae418821c
Rasheed Hendawi <rrash22875@gmail.com>
2024-10-20 01:23:05 +0300

    Fixing Bugs

--- Real-time weather monitoring and reporting service/Program.cs
+++ Real-time weather monitoring and reporting service/Program.cs
@@ -4,13 +4,12 @@ namespace Real_time_weather_monitoring_and_reporting_service
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string configPath = "AppConfigSetting.json";
             InputHandling inputHandling = new InputHandling();
             inputHandling.TakeBotConfiguration(configPath);
         }
-
     }
 
 }
